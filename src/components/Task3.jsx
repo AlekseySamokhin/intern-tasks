9c040c1c9ddaa9d3190b8e8154122322a558c526
AlekseySamokhin <lel.dude01@gmail.com>
2023-02-21 12:08:33 +0300

    added task 3

--- src/components/Task3.jsx
+++ src/components/Task3.jsx
@@ -3,30 +3,31 @@ import { useRef } from 'react';
 import styles from './Task3.module.scss';
 
 const Task3 = () => {
-  const elementRef = useRef();
+  const elementRef = useRef(0);
 
   const width = useMemo(() => {
     const elementWidth = elementRef.current.clientWidth;
     return elementWidth;
   }, [elementRef]);
 
+  console.log(width);
+
   const handleButtonClick = () => {
     if (width >= 100) {
-      elementRef.current.classList.add(styles.blue)
+      elementRef.current.classList.add(styles.blue);
     } else {
-      elementRef.current.classList.add(styles.green)
+      elementRef.current.classList.add(styles.green);
     }
-  }
+  };
 
   return (
-    <div ref={elementRef}>
+    <div>
       <button onClick={handleButtonClick}>Click</button>
       <div className={styles.box} ref={elementRef}>
         Box
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default Task3;
